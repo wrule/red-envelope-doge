9f658aa1204163d4dbfee7519f1f8f9ee8e0612d
scott.gu <jimao@scottgudeMac-mini.local>
2024-12-08 18:13:55 +0800

    RedEnvelopeToken

--- apps/contract/contracts/RedEnvelopeToken.sol
+++ apps/contract/contracts/RedEnvelopeToken.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.8.28;
 
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract GLDToken is ERC20 {
-  constructor(uint256 initialSupply) ERC20("RedEnvelopeToken", "RP") {
-    _mint(msg.sender, initialSupply);
+contract RedEnvelopeToken is ERC20 {
+  constructor() ERC20("RedEnvelope Token", "RP") {
+    _mint(msg.sender, 100_000_000 * (10 ** decimals()));
   }
 }
