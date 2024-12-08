9eafc73383ed765a5b908847d9d92486f8687f5c
scott.gu <jimao@scottgudeMac-mini.local>
2024-12-08 17:46:16 +0800

    sepolia network config

--- apps/contract/hardhat.config.ts
+++ apps/contract/hardhat.config.ts
@@ -1,8 +1,17 @@
-import type { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox-viem";
+import { vars, type HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox-viem';
+
+const ALCHEMY_API_KEY = vars.get('ALCHEMY_API_KEY');
+const SEPOLIA_PRIVATE_KEY = vars.get('SEPOLIA_PRIVATE_KEY');
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.28",
+  solidity: '0.8.28',
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [SEPOLIA_PRIVATE_KEY],
+    },
+  },
 };
 
 export default config;
