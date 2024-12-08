934ae9f3a652d38cfc3821c20a5ee464bd12ccff
scott.gu <jimao@scottgudeMac-mini.local>
2024-12-08 14:56:35 +0800

    RedEnvelope

--- apps/contract/contracts/RedEnvelope.sol
+++ apps/contract/contracts/RedEnvelope.sol
@@ -1,8 +1,17 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.28;
 
-contract RedEnvelope {
-  constructor() {
+import { ERC721URIStorage, ERC721 } from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
+contract RedEnvelope is ERC721URIStorage {
+  uint256 private _nextTokenId;
+
+  constructor() ERC721("RedEnvelopeDoge", "RPDoge") { }
+
+  function awardItem(address player, string memory tokenURI) public returns (uint256) {
+    uint256 tokenId = _nextTokenId++;
+    _mint(player, tokenId);
+    _setTokenURI(tokenId, tokenURI);
+    return tokenId;
   }
 }
