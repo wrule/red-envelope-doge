eedf59f38ac1302f87db7590b42a9c825120d340
scott.gu <jimao@scottgudeMac-mini.local>
2024-12-08 18:26:59 +0800

    ETHERSCAN_API_KEY

--- apps/contract/hardhat.config.ts
+++ apps/contract/hardhat.config.ts
@@ -3,6 +3,7 @@ import '@nomicfoundation/hardhat-toolbox-viem';
 
 const ALCHEMY_API_KEY = vars.get('ALCHEMY_API_KEY');
 const SEPOLIA_PRIVATE_KEY = vars.get('SEPOLIA_PRIVATE_KEY');
+const ETHERSCAN_API_KEY = vars.get('ETHERSCAN_API_KEY');
 
 const config: HardhatUserConfig = {
   solidity: '0.8.28',
@@ -10,6 +11,11 @@ const config: HardhatUserConfig = {
     sepolia: {
       url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
       accounts: [SEPOLIA_PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      sepolia: ETHERSCAN_API_KEY,
     },
   },
 };
