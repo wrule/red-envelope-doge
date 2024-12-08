e7b7326a57a712d8f682c4563582fc7cfcbcb8df
scott.gu <jimao@scottgudeMac-mini.local>
2024-12-08 17:24:22 +0800

    RedEnvelope

--- apps/contract/ignition/modules/RedEnvelope.ts
+++ apps/contract/ignition/modules/RedEnvelope.ts
@@ -2,20 +2,10 @@
 // Learn more about it at https://hardhat.org/ignition
 
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-import { parseEther } from "viem";
 
-const JAN_1ST_2030 = 1893456000;
-const ONE_GWEI: bigint = parseEther("0.001");
-
-const LockModule = buildModule("LockModule", (m) => {
-  const unlockTime = m.getParameter("unlockTime", JAN_1ST_2030);
-  const lockedAmount = m.getParameter("lockedAmount", ONE_GWEI);
-
-  const lock = m.contract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  return { lock };
+const RedEnvelope = buildModule("RedEnvelope", (m) => {
+  const redEnvelope = m.contract("RedEnvelope");
+  return { redEnvelope };
 });
 
-export default LockModule;
+export default RedEnvelope;
