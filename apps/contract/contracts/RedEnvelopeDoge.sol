b58de50c7a364342d2c796fa821ec6b9134f3540
jimao <shihao.gu@perfma.com>
2024-12-10 12:27:21 +0800

    默克尔树验证

--- apps/contract/contracts/RedEnvelopeDoge.sol
+++ apps/contract/contracts/RedEnvelopeDoge.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.28;
 import { Strings } from "@openzeppelin/contracts/utils/Strings.sol";
 import { ERC721 } from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import { Ownable2Step, Ownable } from "@openzeppelin/contracts/access/Ownable2Step.sol";
+import { MerkleProof } from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
 contract RedEnvelopeDoge is ERC721, Ownable2Step {
   using Strings for uint256;
@@ -20,6 +21,7 @@ contract RedEnvelopeDoge is ERC721, Ownable2Step {
   mapping(uint256 => address) private rpdFirst;
 
   error InsufficientValue(uint256 required, uint256 provided);
+  error IllegalUnboxing(uint256 tokenId, bytes32[] proof, address sender);
 
   function mint(
     uint8 num,
@@ -47,5 +49,11 @@ contract RedEnvelopeDoge is ERC721, Ownable2Step {
   function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
     _requireOwned(tokenId);
     return bytes(baseURI).length > 0 ? string.concat(baseURI, "?id=", tokenId.toString()) : "";
+  }
+
+  function unboxing(uint256 tokenId, bytes32[] memory proof) public {
+    if (!MerkleProof.verify(proof, rpdMklRoots[tokenId], keccak256(abi.encodePacked(_msgSender())))) {
+      revert IllegalUnboxing(tokenId, proof, _msgSender());
+    }
   }
 }
